5483fa6000aa5546c5aeb01f90693b2d5657d0a3
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-10-28 10:33:15 -0300

    Usamos leaflet como alternativa a google maps

---
+++ src/js/mapa.js
@@ -0,0 +1,12 @@
+(function() {
+    const lat = -26.8325366;
+    const lng = -65.2962519;
+    const mapa = L.map('mapa').setView([lat, lng ], 14);
+    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
