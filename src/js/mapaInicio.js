92ccb0654cc6501e4ccfd4d767d0c287b0ab95a4
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-12-05 15:39:51 -0300

    limpiando y mostrando los pines del filtrado

--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -5,16 +5,44 @@
 
     let markers = new L.FeatureGroup().addTo(mapa)
 
+
+    let propiedades = [];
+
+
+    // Filtros
+    const filtros = {
+        categoria: '',
+        precio: ''
+    }
+
+    const categoriasSelect = document.querySelector('#categorias');
+    const preciosSelect = document.querySelector('#precios');
+
+
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+
+    // Filtrado de categorias y precios
+    categoriasSelect.addEventListener('change', e=> {
+        filtros.categoria = +e.target.value;
+        filtrarPropiedades();
+    })
+
+    preciosSelect.addEventListener('change', e=> {
+        filtros.precio = +e.target.value;
+        filtrarPropiedades();
+    })
+
+
 
     const obtenerPropiedades = async () => {
         try {
             const url = '/api/propiedades';
             const respuesta = await fetch(url);
             
-            const propiedades = await respuesta.json();
+            propiedades = await respuesta.json();
 
             mostrarPropiedades(propiedades)
 
@@ -24,6 +52,11 @@
     }
 
     const mostrarPropiedades = propiedades => {
+
+        // Limpiar los markers previos
+
+        markers.clearLayers();
+
         propiedades.forEach(propiedad => {
             // Agregar los pines
             const marker = new L.marker([propiedad?.lat, propiedad?.lng], {
@@ -41,6 +74,15 @@
             markers.addLayer(marker)
         });
     }
+
+    const filtrarPropiedades = () => {
+        const resultado = propiedades.filter(filtrarCategorias).filter(filtrarPrecios)
+
+        mostrarPropiedades(resultado)
+    }
+
+    const filtrarCategorias = propiedad => filtros.categoria ? propiedad.categoriaId === filtros.categoria : propiedad
+    const filtrarPrecios = propiedad => filtros.precio ? propiedad.precioId === filtros.precio : propiedad
 
     obtenerPropiedades();
 
