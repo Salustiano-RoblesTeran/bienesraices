19df321c7489449727f015f035646b110f20d072
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-12-10 09:36:40 -0300

    filtrando propiedad por los terminos ingresados

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,3 +1,5 @@
+import { Sequelize } from 'sequelize'
+
 import { Precio, Categoria, Propiedad} from '../models/index.js'
 
 const inicio = async (req, res) => {
@@ -81,9 +83,33 @@ const noEncontrado = (req, res) => {
     })
 }
 
-const buscador = (req, res) => {
+const buscador = async (req, res) => {
+    const { termino } = req.body;
 
-}
+    // Validar que termino no este vacio
+    if(!termino.trim()) {
+        return res.redirect('back')
+    }
+
+    // Consultar las propiedades
+    const propiedades = await Propiedad.findAll({
+        where: {    
+            titulo: {
+                [Sequelize.Op.like]: '%' + termino + '%'
+            }
+        },
+        include: [
+            {model: Precio, as: 'precio'}
+        ]
+    })
+
+    res.render('busqueda', {
+        pagina: 'Resultados de la Busqueda',
+        propiedades,
+        csrfToken:req.csrfToken()
+
+    })
+}   
 
 export {
     inicio,
