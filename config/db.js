42d71f7a32c11d3b237b81fea2729fb465e1e08a
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2025-01-01 11:54:08 -0300

    cambie el puerto al 3607

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const db = new Sequelize(
     process.env.DB_PASSWORD ?? '',
     {
         host: process.env.DB_HOST,
-        port: 3306,
+        port: 3307,
         dialect: 'mysql',
         define: {
             timestamps: true
